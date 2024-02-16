99bbf273f1456e268c5c54f7c1bcceb36f887716
Horraira <abuhorraira1432@gmail.com>
2024-02-17 02:30:36 +0600

    created document for elastic search

---
+++ Blog/documents.py
@@ -0,0 +1,30 @@
+from elasticsearch_dsl import Document, fields
+from django_elasticsearch_dsl.registries import registry
+from .models import Blog
+
+@registry.register_document
+class BlogDocument(Document):
+    author = fields.ObjectField(
+        properties={
+            "username": fields.TextField()
+        }
+    )
+
+    category = fields.ObjectField(
+        properties={
+            "name": fields.TextField()
+        }
+    )
+
+    class Index:
+        name = 'blog_index'
+
+    class Django:
+        model = Blog
+
+        fields = [
+            'id',
+            'title',
+            'author',
+            'category',
+        ]
--- Blog/serializer.py
+++ Blog/serializer.py
@@ -2,7 +2,27 @@ from rest_framework import serializers
 from account.models import *
 from .models import *
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username')
+        read_only = True
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name')
+
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = '__all__'
+        
+class BlogSearchSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=False, read_only=True)
+    category = CategorySerializer(many=False, read_only=True)
+    class Meta:
+        model = Blog
+        fields = ('id', 'author', 'category', 
+                  'title')
+        read_only = True
