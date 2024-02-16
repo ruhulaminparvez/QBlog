33ff77a5f04b338ec9f8a3d84ee5db9f9c725a17
Horraira <abuhorraira1432@gmail.com>
2024-02-16 23:40:13 +0600

    added demo pie chat

--- Blog/views/admin_views.py
+++ Blog/views/admin_views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
-from Blog.serializer import *
-from account.permission import *
 
-# view for Author
-class BlogView(viewsets.ModelViewSet):
-    permission_classes = [BlogOwner]
-    serializer_class = BlogSerializer
-    queryset = Blog.objects.all()
-
-    def get_queryset(self):
-        return Blog.objects.filter(author=self.request.user)
-    
+def my_view(request):
+    context = {'foo': 'bar'}
+    labels = ['asdf','asdf', 'asdf', 'asdf', 'asdf']
+    data = [5, 10, 15, 20, 25]
+    print('asdfasdf')
+    return render(request, 'admin/dashboard.html', {
+        'labels': labels,
+        'data': data,
+    })
 
 
-    
---
+++ Blog/views/author_views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+from Blog.serializer import *
+from account.permission import *
+
+# view for Author
+class BlogView(viewsets.ModelViewSet):
+    permission_classes = [BlogOwner]
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
+
+    def get_queryset(self):
+        return Blog.objects.filter(author=self.request.user)
+    
+
+
+    
